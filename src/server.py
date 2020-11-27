771f2c738e9b49a1c1bd1cee47f0c6703c844c5e
shramikawale <shramikawale@gmail.com>
2020-11-27 15:03:02 +0545

    file updated

--- src/server.py
+++ src/server.py
@@ -3,7 +3,8 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-   return "Hello World!"
+    return "Hello World!"
+
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0') 
+    app.run(debug=False,host= '0.0.0.0', port='80')
