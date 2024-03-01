08ad90e620741c2cf50b1a32ed321ddb3a4c7b6c
Shramik Awale <sawale@express.com>
2024-03-01 23:02:14 +0545

    flask updated

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,6 @@ WORKDIR /code
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY src/ .
-EXPOSE 80
+EXPOSE 81
 
 CMD [ "python", "./server.py" ] 
--- src/server.py
+++ src/server.py
@@ -3,8 +3,8 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "TechAxis Team!!!"
+    return "DevOps Team!!!"
 
 
 if __name__ == "__main__":
-    server.run(debug=False,host= '0.0.0.0', port='80')
+    server.run(debug=False,host= '0.0.0.0', port='81')
